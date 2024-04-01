8b7ee536b714535907d60e3fd093928eb1339b60
hakutaku <qq20099@outlook.com>
2024-04-02 00:15:39 +0800

    upgrade candy

--- pkgs/koboldcpp/default.nix
+++ pkgs/koboldcpp/default.nix
@@ -7,6 +7,7 @@
 , cmake
 , openblas
 , ocl-icd
+, vulkan-loader
 , writeShellApplication
 , makeWrapper
 , pkgs
@@ -37,6 +38,6 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/koboldcpp --prefix PATH : ${lib.makeBinPath [python3 openblas ocl-icd]}
   '';
   # 将 CMake 加入编译环境，用来生成 Makefile
-  nativeBuildInputs = [ pkg-config openblas ocl-icd makeWrapper ];
+  nativeBuildInputs = [ pkg-config vulkan-loader openblas ocl-icd makeWrapper ];
   BuildInputs = [ python3 cmake ];
 }
